248d58f3681b33ec4fc994839eb53b371f1f0868
jxlwqq <jxlwqq@gmail.com>
2018-09-07 05:59:17 +0000

    Apply fixes from StyleCI

--- tests/IdValidatorTest.php
+++ tests/IdValidatorTest.php
@@ -30,27 +30,27 @@ class IdValidatorTest extends \PHPUnit_Framework_TestCase
     {
         $idValidator = new IdValidator();
         $this->assertEquals([
-            'addressCode'  => '440308',
-            'address'      => '广东省深圳市盐田区',
-            'birthdayCode' => '1999-01-10',
+            'addressCode'   => '440308',
+            'address'       => '广东省深圳市盐田区',
+            'birthdayCode'  => '1999-01-10',
             'constellation' => '水瓶座',
             'chineseZodiac' => '卯兔',
-            'sex'          => 1,
-            'length'       => 18,
-            'checkBit'     => '2', ],
+            'sex'           => 1,
+            'length'        => 18,
+            'checkBit'      => '2', ],
             $idValidator->getInfo('440308199901101512'));
         $this->assertEquals(false, $idValidator->isValid('440308199901101513'));
 
         $idValidator = new IdValidator();
         $this->assertEquals([
-            'addressCode'  => '610104',
-            'address'      => '陕西省西安市莲湖区',
-            'birthdayCode' => '1962-09-27',
+            'addressCode'   => '610104',
+            'address'       => '陕西省西安市莲湖区',
+            'birthdayCode'  => '1962-09-27',
             'constellation' => '天秤座',
             'chineseZodiac' => '寅虎',
-            'sex'          => 0,
-            'length'       => 15,
-            'checkBit'     => '', ],
+            'sex'           => 0,
+            'length'        => 15,
+            'checkBit'      => '', ],
             $idValidator->getInfo('610104620927690'));
         $this->assertEquals(false, $idValidator->isValid('610104620932690'));
     }
