1791114e1b661d63bac02a5d61b6ebdb66e6ef9f
jxlwqq <jxlwqq@gmail.com>
2018-10-25 14:57:00 +0800

    添加测试用例

--- tests/IdValidatorTest.php
+++ tests/IdValidatorTest.php
@@ -31,6 +31,7 @@ class IdValidatorTest extends TestCase
         $this->assertFalse($this->idValidator->isValid('440308199901101513')); // 验证码不合法
         $this->assertFalse($this->idValidator->isValid('610104620932690'));    // 验证码不合法
         $this->assertTrue($this->idValidator->isValid('440308199901101512'));
+        $this->assertTrue($this->idValidator->isValid('500154199301135886'));
         $this->assertTrue($this->idValidator->isValid('610104620927690'));
     }
 
@@ -52,30 +53,77 @@ class IdValidatorTest extends TestCase
     {
         $this->assertEquals(
             [
-            'addressCode'   => '440308',
-            'abandoned'     => 0,
-            'address'       => '广东省深圳市盐田区',
-            'birthdayCode'  => '1999-01-10',
-            'constellation' => '摩羯座',
-            'chineseZodiac' => '卯兔',
-            'sex'           => 1,
-            'length'        => 18,
-            'checkBit'      => '2', ],
+                'addressCode' => '440308',
+                'abandoned' => 0,
+                'address' => '广东省深圳市盐田区',
+                'birthdayCode' => '1999-01-10',
+                'constellation' => '摩羯座',
+                'chineseZodiac' => '卯兔',
+                'sex' => 1,
+                'length' => 18,
+                'checkBit' => '2',],
             $this->idValidator->getInfo('440308199901101512')
         );
+
+        $this->assertEquals(
+            [
+                'addressCode' => '362324',
+                'abandoned' => 1,
+                'address' => '江西省宜春地区丰城县',
+                'birthdayCode' => '1980-01-01',
+                'constellation' => '摩羯座',
+                'chineseZodiac' => '申猴',
+                'sex' => 1,
+                'length' => 18,
+                'checkBit' => '4',
+            ],
+            $this->idValidator->getInfo('362324198001010014')
+        );
+
+        $this->assertEquals(
+            [
+                'addressCode' => '362324',
+                'abandoned' => 1,
+                'address' => '江西省宜春地区丰城县',
+                'birthdayCode' => '1981-01-01',
+                'constellation' => '摩羯座',
+                'chineseZodiac' => '酉鸡',
+                'sex' => 1,
+                'length' => 18,
+                'checkBit' => '1',],
+            $this->idValidator->getInfo('362324198101010011')
+        );
+
+
+        $this->assertEquals(
+            [
+                'addressCode' => '362324',
+                'abandoned' => 1,
+                'address' => '江西省上饶地区铅山县',
+                'birthdayCode' => '1982-01-01',
+                'constellation' => '摩羯座',
+                'chineseZodiac' => '戌狗',
+                'sex' => 1,
+                'length' => 18,
+                'checkBit' => '9',
+            ],
+            $this->idValidator->getInfo('362324198201010019')
+        );
+
+
         $this->assertFalse($this->idValidator->isValid('440308199901101513'));
 
         $this->assertEquals(
             [
-            'addressCode'   => '610104',
-            'abandoned'     => 0,
-            'address'       => '陕西省西安市莲湖区',
-            'birthdayCode'  => '1962-09-27',
-            'constellation' => '天秤座',
-            'chineseZodiac' => '寅虎',
-            'sex'           => 0,
-            'length'        => 15,
-            'checkBit'      => '', ],
+                'addressCode' => '610104',
+                'abandoned' => 0,
+                'address' => '陕西省西安市莲湖区',
+                'birthdayCode' => '1962-09-27',
+                'constellation' => '天秤座',
+                'chineseZodiac' => '寅虎',
+                'sex' => 0,
+                'length' => 15,
+                'checkBit' => '',],
             $this->idValidator->getInfo('610104620927690')
         );
         $this->assertFalse($this->idValidator->isValid('610104620932690'));
