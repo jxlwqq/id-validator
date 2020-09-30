868ab2f5da1167dca76fa8e1a45878948957aa3a
jxlwqq <jxlwqq@gmail.com>
2020-09-30 09:12:58 +0800

    fix test error

--- tests/IdValidatorTest.php
+++ tests/IdValidatorTest.php
@@ -13,48 +13,41 @@ use PHPUnit\Framework\TestCase;
 
 class IdValidatorTest extends TestCase
 {
-    /**
-     * @var \Jxlwqq\IdValidator\IdValidator
-     */
-    private $idValidator;
-
-    protected function setUp()
-    {
-        $this->idValidator = new IdValidator();
-    }
-
     public function testIsValid()
     {
-        $this->assertFalse($this->idValidator->isValid('44030819990110'));     // 号码位数不合法
-        $this->assertFalse($this->idValidator->isValid('111111199901101512')); // 地址码不合法
-        $this->assertFalse($this->idValidator->isValid('440308199902301512')); // 出生日期码不合法
-        $this->assertFalse($this->idValidator->isValid('440308199901101513')); // 验证码不合法
-        $this->assertFalse($this->idValidator->isValid('610104620932690'));    // 出生日期码不合法
-        $this->assertFalse($this->idValidator->isValid('11010119900307867X')); // 校验位不合法
-        $this->assertTrue($this->idValidator->isValid('110101199003078670'));
-        $this->assertTrue($this->idValidator->isValid('440308199901101512'));
-        $this->assertTrue($this->idValidator->isValid('500154199301135886'));
-        $this->assertTrue($this->idValidator->isValid('610104620927690'));
-        $this->assertTrue($this->idValidator->isValid('810000199408230021')); // 港澳居民居住证 18 位
-        $this->assertTrue($this->idValidator->isValid('830000199201300022')); // 台湾居民居住证 18 位
+        $idValidator = new IdValidator();
+        $this->assertFalse($idValidator->isValid('44030819990110'));     // 号码位数不合法
+        $this->assertFalse($idValidator->isValid('111111199901101512')); // 地址码不合法
+        $this->assertFalse($idValidator->isValid('440308199902301512')); // 出生日期码不合法
+        $this->assertFalse($idValidator->isValid('440308199901101513')); // 验证码不合法
+        $this->assertFalse($idValidator->isValid('610104620932690'));    // 出生日期码不合法
+        $this->assertFalse($idValidator->isValid('11010119900307867X')); // 校验位不合法
+        $this->assertTrue($idValidator->isValid('110101199003078670'));
+        $this->assertTrue($idValidator->isValid('440308199901101512'));
+        $this->assertTrue($idValidator->isValid('500154199301135886'));
+        $this->assertTrue($idValidator->isValid('610104620927690'));
+        $this->assertTrue($idValidator->isValid('810000199408230021')); // 港澳居民居住证 18 位
+        $this->assertTrue($idValidator->isValid('830000199201300022')); // 台湾居民居住证 18 位
     }
 
     public function testFakeId()
     {
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId()));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(false)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '上海市', '2000', 1)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '黄浦区', '2001', 0)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '江苏省', '200001', 1)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '南京市', '2002', 0)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '秦淮区', '2003', 0)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '台湾省', '20181010', 0)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '香港特别行政区', '20181010', 1)));
-        $this->assertTrue($this->idValidator->isValid($this->idValidator->fakeId(true, '澳门特别行政区', '20181111', 0)));
+        $idValidator = new IdValidator();
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId()));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(false)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '上海市', '2000', 1)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '黄浦区', '2001', 0)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '江苏省', '200001', 1)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '南京市', '2002', 0)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '秦淮区', '2003', 0)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '台湾省', '20181010', 0)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '香港特别行政区', '20181010', 1)));
+        $this->assertTrue($idValidator->isValid($idValidator->fakeId(true, '澳门特别行政区', '20181111', 0)));
     }
 
     public function testGetInfo()
     {
+        $idValidator = new IdValidator();
         $this->assertEquals(
             [
                 'addressCode'   => '440308',
@@ -67,7 +60,7 @@ class IdValidatorTest extends TestCase
                 'sex'           => 1,
                 'length'        => 18,
                 'checkBit'      => '2', ],
-            $this->idValidator->getInfo('440308199901101512')
+            $idValidator->getInfo('440308199901101512')
         );
 
         $this->assertEquals(
@@ -83,7 +76,7 @@ class IdValidatorTest extends TestCase
                 'length'        => 18,
                 'checkBit'      => '4',
             ],
-            $this->idValidator->getInfo('362324198001010014')
+            $idValidator->getInfo('362324198001010014')
         );
 
         $this->assertEquals(
@@ -98,7 +91,7 @@ class IdValidatorTest extends TestCase
                 'sex'           => 1,
                 'length'        => 18,
                 'checkBit'      => '1', ],
-            $this->idValidator->getInfo('362324198101010011')
+            $idValidator->getInfo('362324198101010011')
         );
 
         $this->assertEquals(
@@ -114,10 +107,10 @@ class IdValidatorTest extends TestCase
                 'length'        => 18,
                 'checkBit'      => '9',
             ],
-            $this->idValidator->getInfo('362324198201010019')
+            $idValidator->getInfo('362324198201010019')
         );
 
-        $this->assertFalse($this->idValidator->isValid('440308199901101513'));
+        $this->assertFalse($idValidator->isValid('440308199901101513'));
 
         $this->assertEquals(
             [
@@ -131,13 +124,14 @@ class IdValidatorTest extends TestCase
                 'sex'           => 0,
                 'length'        => 15,
                 'checkBit'      => '', ],
-            $this->idValidator->getInfo('610104620927690')
+            $idValidator->getInfo('610104620927690')
         );
-        $this->assertFalse($this->idValidator->isValid('610104620932690'));
+        $this->assertFalse($idValidator->isValid('610104620932690'));
     }
 
     public function testUpgradeId()
     {
-        $this->assertEquals('610104196209276908', $this->idValidator->upgradeId('610104620927690'));
+        $idValidator = new IdValidator();
+        $this->assertEquals('610104196209276908', $idValidator->upgradeId('610104620927690'));
     }
 }
