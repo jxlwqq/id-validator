40d092849f4b10980ea2d0ceb6951d2b5f49da36
jxlwqq <jxlwqq@gmail.com>
2021-04-04 11:23:54 +0800

    opt: remove timeline check.

--- src/Helper.php
+++ src/Helper.php
@@ -10,9 +10,9 @@ trait Helper
     /**
      * 获取地址码信息.
      *
-     * @param string $addressCode  地址码
+     * @param string $addressCode 地址码
      * @param string $birthdayCode 出生日期码
-     * @param bool   $strictMode   是否启动严格模式检查
+     * @param bool $strictMode 是否启动严格模式检查
      *
      * @return bool|mixed|string
      */
@@ -20,12 +20,12 @@ trait Helper
     {
         $addressInfo = [
             'province' => '',
-            'city'     => '',
+            'city' => '',
             'district' => '',
         ];
 
         // 省级信息
-        $provinceAddressCode = substr($addressCode, 0, 2).'0000';
+        $provinceAddressCode = substr($addressCode, 0, 2) . '0000';
         $addressInfo['province'] = $this->_getAddress($provinceAddressCode, $birthdayCode, $strictMode);
 
         $firstCharacter = substr($addressCode, 0, 1); // 用于判断是否是港澳台居民居住证（8字开头）
@@ -36,21 +36,24 @@ trait Helper
         }
 
         // 市级信息
-        $cityAddressCode = substr($addressCode, 0, 4).'00';
+        $cityAddressCode = substr($addressCode, 0, 4) . '00';
         $addressInfo['city'] = $this->_getAddress($cityAddressCode, $birthdayCode, $strictMode);
 
         // 县级信息
         $addressInfo['district'] = $this->_getAddress($addressCode, $birthdayCode, $strictMode);
 
+        // 这里不判断市级信息的原因：
+        // 1）直辖市，无市级信息
+        // 2）省直辖县或县级市，无市级信息
         return (empty($addressInfo['district']) or empty($addressInfo['province'])) ? false : $addressInfo;
     }
 
     /**
      * 获取省市区地址码.
      *
-     * @param string $addressCode  地址码
+     * @param string $addressCode 地址码
      * @param string $birthdayCode 出生日期码
-     * @param bool   $strictMode   是否启动严格模式检查
+     * @param bool $strictMode 是否启动严格模式检查
      *
      * @return string
      */
@@ -73,11 +76,10 @@ trait Helper
             if (empty($address) and !$strictMode) {
                 foreach ($timeline as $key => $val) {
                     // 由于较晚申请户口或身份证等原因，导致会出现地址码正式启用于2000年，但实际1999年出生的新生儿，由于晚了一年报户口，导致身份证上的出生年份早于地址码正式启用的年份
-                    $end_year = $val['end_year'] != '' ? $val['end_year'] : '9999';
-                    if ($year <= $end_year) {
-                        $address = $val['address'];
-                        break;
-                    }
+                    // 由于某些地区的地址码已经废弃，但是实际上在之后的几年依然在使用
+                    // 这里就不做时间判断了
+                    $address = $val['address'];
+                    break;
                 }
             }
         } else {
@@ -117,12 +119,12 @@ trait Helper
      */
     private function _getConstellation($birthdayCode)
     {
-        $constellationList = include __DIR__.'/../data/constellation.php';
-        $month = (int) substr($birthdayCode, 4, 2);
-        $day = (int) substr($birthdayCode, 6, 2);
+        $constellationList = include __DIR__ . '/../data/constellation.php';
+        $month = (int)substr($birthdayCode, 4, 2);
+        $day = (int)substr($birthdayCode, 6, 2);
 
         $start_date = $constellationList[$month]['start_date'];
-        $start_day = (int) explode('-', $start_date)[1];
+        $start_day = (int)explode('-', $start_date)[1];
 
         if ($day < $start_day) {
             $tmp_month = $month == 1 ? 12 : $month - 1;
@@ -142,7 +144,7 @@ trait Helper
      */
     private function _getChineseZodiac($birthdayCode)
     {
-        $chineseZodiacList = include __DIR__.'/../data/chineseZodiac.php';
+        $chineseZodiacList = include __DIR__ . '/../data/chineseZodiac.php';
         $start = 1900; // 子鼠
         $end = substr($birthdayCode, 0, 4);
         $key = ($end - $start) % 12;
